ddd24d9315ff7a71c16067e1b284734490c535f6
Pankaj Sharma <grgnld3970@gmail.com>
2025-07-25 13:01:03 +0530

    first nextjs & ts project- database setup done

---
+++ lib/db.ts
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if(!MONGODB_URI) {
+    throw new Error("Please define mongoose url for connection in env");
+}
+
+let cached = global.mongoose;
+
+if(!cached) {
+    cached = global.mongoose = {conn: null, promise: null};
+}
+
+export async function connectToDatabase() {
+    if(cached.conn) {
+        return cached.conn;
+    }
+
+    const opts = {
+        bufferCommand: true,
+        maxPoolSize: 10
+    }
+    if(!cached.promise) {
+        mongoose
+        .connect(MONGODB_URI, opts)
+        .then(() => mongoose.Connection);
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    }catch(error) {
+        cached.promise = null;
+        throw error;
+    }
+}
+
---
+++ models/video.model.ts
@@ -0,0 +1,60 @@
+import {Schema, models, model} from 'mongoose';
+
+export const VIDEO_DIMENSIONS = {
+    height: 1920,
+    width: 1080
+} as const;
+
+export interface IVideo{
+    title: string;
+    description: string;
+    videoUrl: string;
+    thumbnailUrl: string;
+    controls?: boolean;
+    transformations?: {
+        height: number;
+        width: number;
+        quality?: number;
+    }
+};
+
+const videoSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    videoUrl: {
+        type: String,
+        required: true
+    },
+    thumbnailUrl: {
+        type: String,
+        required: true
+    },
+    controls: {
+        type: Boolean,
+        default: true
+    },
+    tranformations: {
+        height: {
+            type: Number,
+            default: VIDEO_DIMENSIONS.height
+        },
+        width: {
+            type: Number,
+            default: VIDEO_DIMENSIONS.width
+        },
+        quality: {
+            type: Number,
+            min: 1,
+            max: 100
+        }
+    }
+},{timestamps: true})
+
+const Video = models?.Video || model<IVideo>("Video", videoSchema);
+export default Video;
